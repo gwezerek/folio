3fcda5c7a1c8bc4dbefdf72c0fcf9995117b376b
Gus Wezerek <gwezerek@gmail.com>
2013-09-15 19:41:54 -0400

    Test grunt site up and running.

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,13 +14,6 @@ module.exports = function(grunt) {
       }
     },
 
-    // shell: {
-    //   jekyll: {
-    //     command: 'rm -rf _site/*; jekyll',
-    //     stdout: true
-    //   }
-    // },
-
     compass: {
       dev: {
         options: {
@@ -31,169 +24,32 @@ module.exports = function(grunt) {
     },
 
     watch: {
-      options: {
-        livereload: true
-      },
       sass: {
         files: ['sass/**/*.scss'],
         tasks: ['compassCopy']
+      },
+      files: [
+        '_includes/*.html',
+        '_layouts/*.html',
+        '_posts/*.html',
+        '_config.yml',
+        'index.html'
+      ],
+      options: {
+        livereload: true,
+        atBegin: true,
+        interrupt: true,
+        debounceDelay: 300
       }
-      // jekyllSources: {
-      //   files: [
-      //     // capture all except css
-      //     '*.html', '*.yml', 'js/**.js', '_posts/**','_includes/**'
-      //   ]
-      //   // tasks: ['shell:jekyll']
-      // }
     }
-
-    // connect: {
-    //   server: {
-    //     options: {
-    //       base: '_site/',
-    //       port: 9009
-    //     }
-    //   }
-    // },
-
-    // open: {
-    //   server: {
-    //     path: 'http://localhost:<%= connect.server.options.port %>/'
-    //   }
-    // }
 
   });
 
   // less watch
   grunt.registerTask('compassCopy', ['compass:dev', 'copy:css']);
 
-  grunt.registerTask('server', [
-    // 'connect:server',
-    // 'open:server',
-    'watch'
-  ]);
-
   // Default task.
-  grunt.registerTask('default', 'server');
+  grunt.registerTask('default', 'watch');
 
 };
-
-
-
-      // sass: {
-      //   files: ['sass/**/*.scss'],
-      //   tasks: ['compassCopy']
-      // }
-
-
-// module.exports = function(grunt) {
-
-//     // Bring in all the plugins listed in the packages.json file and load their tasks
-//     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-
-//     grunt.initConfig({
-
-//       copy: {
-//         css : {
-//           files: {
-//             '_site/css/site.css': 'css/site.css'
-//           }
-//         }
-//       },
-
-//       shell: {
-//           // jekyllBuild: {
-//           //     command: 'jekyll build'
-//           // },
-//           // jekyllServe: {
-//           //     command: 'jekyll serve'
-//           // }
-//           jekyll: {
-//             command: 'rm -rf _site/*; jekyll',
-//             stdout: true
-//           }
-//       },
-//       compass: {
-//         dev: {
-//           options: {
-//             // 2. Compiling task
-//             sassDir: 'sass',
-//             cssDir: 'css'
-//           }
-//         }
-//       },
-
-
-//       // watch: {
-//       //   sass: {
-//       //     // 1. We watch and compile sass files as normal but don't live reload here
-//       //     files: ['sass/**/*.scss'],
-//       //     tasks: ['compass']
-//       //   },
-//       //   files: [
-//       //       '_includes/*.html',
-//       //       '_layouts/*.html',
-//       //       '_posts/*.html',
-//       //       '_config.yml',
-//       //       'index.html'
-//       //   ],
-//       //   tasks: ['shell:jekyllBuild', 'shell:jekyllServe'],
-//       //   livereload: {
-//       //     // 3. Here we watch the css file the compass task will compile to
-//       //     //    These files are sent to the live reload server after compass compiles to them
-//       //     //    atBegin reloads upon the terminal command and not just on save 
-//       //     options: { livereload: true, atBegin: true, interrupt: true },
-//       //     files: ['css/site.css'],
-//       //   }
-//       // }
-
-//       watch: {
-//         options: {
-//           livereload: true
-//         },
-//         compass: {
-//           files: ['sass/**/*.scss'],
-//           tasks: ['sassCopy']
-//         },
-//         jekyllSources: {
-//           files: [
-//             // capture all except css
-//             '*.html', '*.yml', 'js/**.js', '_posts/**','_includes/**'
-//           ],
-//           tasks: ['shell:jekyll']
-//         }
-//       },
-
-
-
-//       connect: {
-//         server: {
-//           options: {
-//             base: '_site/',
-//             port: 4000
-//           }
-//         }
-//       },
-//       open: {
-//         server: {
-//           path: 'http://localhost:<%= connect.server.options.port %>/'
-//         }
-//       }
-
-//     });
- 
-//     grunt.registerTask('sassCopy', ['compass:dev', 'copy:css']);
-
-//     grunt.registerTask('server', [
-//       // 'connect:server',
-//       // 'open:server',
-//       'watch'
-//     ]);
-
-//     // Default task.
-//     grunt.registerTask('default', 'server');
-
-//   // the default task can be run just by typing "grunt" on the command line
-//     // grunt.registerTask('default', 'watch');
-// };
 
