9b7c544b99ade33c49afb15546d4490cb284a01b
Gus Wezerek <gus@upstatement.com>
2013-09-18 19:21:32 -0400

    Simplifying cat-btn functions.

--- js/main.js
+++ js/main.js
@@ -20,11 +20,33 @@
 	// ON CATEGORY EXPAND
 
 	// Expand the first post and post wrapper
-	$('.cat-expand').on('click', function() {
-		var wrap = $(this).siblings('.post-wrap-collapsed');
+	// $('.cat-expand').on('click', function() {
+	// 	var $this = $(this),
+	// 		wrap = $(this).siblings('.post-wrap-collapsed');
 
-		wrap.removeClass('post-wrap-collapsed');
-		wrap.find('.post-collapsed').removeClass('post-collapsed');
+	// 	wrap.removeClass('post-wrap-collapsed').addClass('post-wrap-expanded');
+	// 	wrap.find('.post-collapsed').removeClass('post-collapsed').addClass('post-expanded');
+	// 	$this.removeClass('cat-expand').addClass('cat-collapse');
+	// 	console.log($('.cat-collapse'));
+	// });
+
+	$('.cat-btn').on('click', function() {
+		var $this = $(this),
+			wrap = $(this).siblings('.post-wrap');
+			// wrapExpanded = $(this).siblings('.post-wrap-expanded');
+
+		if ($this.hasClass('cat-expand')) {
+			wrap.toggleClass('post-wrap-collapsed');
+			wrap.find('.post').toggleClass('post-collapsed');
+			$this.toggleClass('cat-expand');
+		} else {
+
+		}
+
+		// wrap.removeClass('post-wrap-collapsed').addClass('post-wrap-expanded');
+		// wrap.find('.post-collapsed').removeClass('post-collapsed').addClass('post-expanded');
+		// $this.removeClass('cat-expand').addClass('cat-collapse');
+		// console.log($('.cat-collapse'));
 	});
 
 	// Load all images in set
@@ -38,6 +60,14 @@
 	// ON CATEGORY CLOSE
 
 	// Add .post-collapsed to current post
+	// $('.cat-collapse').on('click', function() {
+	// 	var $this = $(this),
+	// 		wrap = $(this).siblings('.post-wrap-expanded');
+
+	// 	wrap.removeClass('post-wrap-expanded').addClass('post-wrap-collapsed');
+	// 	wrap.find('.post-expanded').removeClass('post-expanded').addClass('post-collapsed');
+	// 	$this.removeClass('cat-collapse').addClass('cat-expand');
+	// });
 
 	// Change category-collapse button to category-expand
 
