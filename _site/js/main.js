32a9f22f55d9664cd06622e4945c027ce76a589c
Gus Wezerek <gwezerek@gmail.com>
2013-09-19 08:58:46 -0400

    Added next and prev post functionality.

--- _site/js/main.js
+++ _site/js/main.js
@@ -20,59 +20,52 @@
 	// ON CATEGORY EXPAND
 
 	// Expand the first post and post wrapper
-	// $('.cat-expand').on('click', function() {
-	// 	var $this = $(this),
-	// 		wrap = $(this).siblings('.post-wrap-collapsed');
-
-	// 	wrap.removeClass('post-wrap-collapsed').addClass('post-wrap-expanded');
-	// 	wrap.find('.post-collapsed').removeClass('post-collapsed').addClass('post-expanded');
-	// 	$this.removeClass('cat-expand').addClass('cat-collapse');
-	// 	console.log($('.cat-collapse'));
-	// });
 
 	$('.cat-btn').on('click', function() {
-		var $this = $(this),
-			wrap = $(this).siblings('.post-wrap');
-			// wrapExpanded = $(this).siblings('.post-wrap-expanded');
+		var $this 	= $(this),
+			wrap 	= $(this).siblings('.post-wrap'),
+			wrapValue = wrap.data('index');
 
 		if ($this.hasClass('cat-expand')) {
 			wrap.toggleClass('post-wrap-collapsed');
-			wrap.find('.post').toggleClass('post-collapsed');
 			$this.toggleClass('cat-expand');
+			wrap.find('.post[data-index='+wrapValue+']').slideDown();
 		} else {
-
+			wrap.find('.post').slideUp();
+			$this.toggleClass('cat-expand');
+			setTimeout (function(){
+				wrap.toggleClass('post-wrap-collapsed')
+			}, 300);
 		}
 
-		// wrap.removeClass('post-wrap-collapsed').addClass('post-wrap-expanded');
-		// wrap.find('.post-collapsed').removeClass('post-collapsed').addClass('post-expanded');
-		// $this.removeClass('cat-expand').addClass('cat-collapse');
-		// console.log($('.cat-collapse'));
 	});
 
 	// Load all images in set
 
-	// Change cat-expand button to cat-collapse
-
-
-
-
-
-	// ON CATEGORY CLOSE
-
-	// Add .post-collapsed to current post
-	// $('.cat-collapse').on('click', function() {
-	// 	var $this = $(this),
-	// 		wrap = $(this).siblings('.post-wrap-expanded');
-
-	// 	wrap.removeClass('post-wrap-expanded').addClass('post-wrap-collapsed');
-	// 	wrap.find('.post-expanded').removeClass('post-expanded').addClass('post-collapsed');
-	// 	$this.removeClass('cat-collapse').addClass('cat-expand');
-	// });
-
-	// Change category-collapse button to category-expand
 
 
 	// ON NEXT CLICK
+
+	$('.post-nav').on('click', function() {
+		var  wrap 	= $(this).siblings('.post-wrap'),
+			wrapValue = wrap.data('index');
+			// wrapExpanded = $(this).siblings('.post-wrap-expanded');
+
+			// console.log($this.closest('.post'));
+			$(this).closest('.post').hide();
+
+
+			// $(this).closest('.post').next().show();
+
+
+
+		if ($(this).hasClass('post-prev')) {
+			$(this).closest('.post').prev().show();
+		} else {
+			$(this).closest('.post').next().show();
+		}
+
+	});
 
 	// Add .post-collapsed to current .post
 
