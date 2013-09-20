0474cf7853e0b6203404680bbd5b9b78f3467c64
Gus Wezerek <gus@upstatement.com>
2013-09-20 18:30:35 -0400

    Added one solution for iterating through posts, a "current" class.

--- js/main.js
+++ js/main.js
@@ -1,88 +1,112 @@
-
 (function($) {
 
-	$(function() {
-		// $('.post-wrap').
-	});
-	
-	$('.lazy').lazyload({
-		
-	});
+    $(function() {
+        // $('.post-wrap').
+    });
+
+    $('.lazy').lazyload({
+
+    });
 
 
-	// ON READY
+    // ON READY
 
-	// Load first image of each set
-
-
-
-
-	// ON CATEGORY EXPAND
-
-	// Expand the first post and post wrapper
-
-	$('.cat-btn').on('click', function() {
-		var $this 	= $(this),
-			wrap 	= $(this).siblings('.post-wrap'),
-			wrapValue = wrap.data('index');
-
-		if ($this.hasClass('cat-expand')) {
-			wrap.toggleClass('post-wrap-collapsed');
-			$this.toggleClass('cat-expand');
-			wrap.find('.post[data-index='+wrapValue+']').slideDown();
-		} else {
-			wrap.find('.post').slideUp();
-			$this.toggleClass('cat-expand');
-			setTimeout (function(){
-				wrap.toggleClass('post-wrap-collapsed')
-			}, 300);
-		}
-
-	});
-
-	$('.cat-btn-inside').on('click', function() {
-		var $this 	= $(this),
-			wrap 	= $this.closest('.post-wrap');
-
-			$this.closest('.post').slideUp();
-			setTimeout (function(){
-				wrap.toggleClass('post-wrap-collapsed')
-			}, 300);
-	});
-
-	// Load all images in set
+    // Load first image of each set
 
 
 
-	// ON NEXT CLICK
 
-	$('.post-nav').on('click', function() {
-		var  wrap 	= $(this).siblings('.post-wrap'),
-			wrapValue = wrap.data('index');
-			// wrapExpanded = $(this).siblings('.post-wrap-expanded');
+    // ON CATEGORY EXPAND
+    // EXAMPLE WITH CURRENT CLASS
 
-			// console.log($this.closest('.post'));
-			$(this).closest('.post').hide();
+    $('.cat-btn').on('click', function() {
+        var $this = $(this),
+            wrap = $(this).siblings('.post-wrap');
+
+        if ($this.hasClass('cat-expand')) {
+            wrap.toggleClass('post-wrap-collapsed');
+            $this.toggleClass('cat-expand');
+            wrap.find('.current').slideDown();
+        } else {
+            wrap.find('.post').slideUp();
+            $this.toggleClass('cat-expand');
+            setTimeout(function() {
+                wrap.toggleClass('post-wrap-collapsed')
+            }, 300);
+        }
+    });
+
+    $('.cat-btn-inside').on('click', function() {
+        var $this = $(this),
+            wrap = $this.closest('.post-wrap');
+
+        $this.closest('.post').slideUp();
+        setTimeout(function() {
+            wrap.toggleClass('post-wrap-collapsed')
+        }, 300);
+    });
 
 
-			// $(this).closest('.post').next().show();
+    // ON NEXT CLICK
+
+    $('.post-nav').on('click', function() {
+    	var post = $(this).closest('.post');
+
+        post.hide().removeClass('current');
+
+        if ($(this).hasClass('post-prev')) {
+            post.prev().show().addClass('current');
+        } else {
+            post.next().show().addClass('current');
+        }
+    });
 
 
 
-		if ($(this).hasClass('post-prev')) {
-			$(this).closest('.post').prev().show();
-		} else {
-			$(this).closest('.post').next().show();
-		}
+    // EXAMPLE WITH INDEX VAR
 
-	});
+    // $('.cat').each(function(index, val) {
+    // 	var postLen = $('.posts').length,
+    // 		current = 1;
 
-	// Add .post-collapsed to current .post
+    // 	$('.cat-btn').on('click', function() {
+    // 		var $this 	= $(this),
+    // 			wrap 	= $(this).siblings('.post-wrap');
+    // 			// wrapValue 	= wrap.data('index');
 
-	// Remove .post-collapsed from next .post
+    // 		if ($this.hasClass('cat-expand')) {
+    // 			wrap.toggleClass('post-wrap-collapsed');
+    // 			$this.removeClass('cat-expand');
+    // 			wrap.find('.post').filter('[data-index='+current+']').slideDown();
+    // 			console.log('h');
+    // 		} else {
+    // 			// wrap.find('.post').slideUp();
+    // 			// $this.toggleClass('cat-expand');
+    // 			// console.log(current);
+    // 			// setTimeout (function(){
+    // 			// 	wrap.toggleClass('post-wrap-collapsed')
+    // 			// }, 300);
+    // 		}
+    // 	});
 
-	// On last post, run on category expand for next category
+    // 	// console.log($('post-nav'));
 
+    // 	$('.post-nav').on('click', function() {
+    // 		var  wrap = $(this).siblings('.post-wrap');
+
+    // 			$(this).closest('.post').hide();
+
+    // 		if ($(this).hasClass('post-prev')) {
+    // 			$(this).closest('.post').prev().show();
+    // 			wrap.data("index");
+    // 			current --;
+    // 		} else {
+    // 			$(this).closest('.post').next().show();
+    // 			current ++;
+    // 		}
+    // 	});
+
+    // }
 
 
 })(jQuery);
